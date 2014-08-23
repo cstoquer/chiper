521714933966b90fcdfd3568618fb75445176827
Cedric Stoquer <cstoquer@wizcorp.jp>
2014-08-23 18:41:37 +0900

    cleanup and doc

--- src/PulseSynth.h
+++ src/PulseSynth.h
@@ -10,63 +10,83 @@
 class PulseSynth {
 
 private:
-	float          tune;
-	float          note;
-	bool           mute;
-	float          width;
 
-	OscPulse       osc;
-	NoteBuffer     noteBuffer;
-	ArpController  arpController;
+float          tune;
+float          note;
+bool           mute;
+float          width;
 
-	void setNote(float n) {
-		if (n == note) return;
-		note = n;
-		osc.freq = noteToFreq(note + tune);
-	}
+OscPulse       osc;
+NoteBuffer     noteBuffer;
+ArpController  arpController;
+
+/**▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ * set note.
+ */
+void setNote(float n) {
+	if (n == note) return;
+	note = n;
+	osc.freq = noteToFreq(note + tune);
+}
 
 public:
 
-	PulseSynth() {
-		tune = 0.0;
-		note = 36.0;
-		mute = true;
-		osc.width = 0.25;
-		arpController.freq = 40.0;
-	}
+/**▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ * Constructor.
+ */
+PulseSynth() {
+	tune = 0.0;
+	note = 36.0;
+	mute = true;
+	osc.width = 0.25;
+	arpController.freq = 40.0;
+}
 
-    float tic() {
-    	if (noteBuffer.polyphony > 0) {
-			if (arpController.tic()) {
-				setNote(noteBuffer.buffer[arpController.out]);
-			}
+/**▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ * compute one audio frame.
+ */
+float tic() {
+	if (noteBuffer.polyphony > 0) {
+		if (arpController.tic()) {
+			setNote(noteBuffer.buffer[arpController.out]);
 		}
-
-    	if (mute) return 0.0;
-		osc.tic();
-		return osc.out;
 	}
+	if (mute) return 0.0;
+	osc.tic();
+	return osc.out;
+}
 
-	void setWidth(float w) {
-		osc.width = w;
-	}
+/**▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ * set oscillator width
+ */
+void setWidth(float w) {
+	osc.width = w;
+}
 
-	void setTune(float t) {
-		tune = t;
-		osc.freq = noteToFreq(note + tune);
-	}
+/**▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ * set synth global tuning
+ */
+void setTune(float t) {
+	tune = t;
+	osc.freq = noteToFreq(note + tune);
+}
 
-	void noteEvent(float n, bool p) {
-		if (p) {
-			if (noteBuffer.addNote(n)) setNote(n);
-			mute = false;
-		} else {
-			n = noteBuffer.delNote(n);
-			if (n == 0) mute = true;
-			else setNote(n);
-		}
-		arpController.setLength(noteBuffer.polyphony);
+/**▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ * note envent (noteOn / noteOff)
+ */
+void noteEvent(float n, bool p) {
+	if (p) {
+		if (noteBuffer.addNote(n)) setNote(n);
+		mute = false;
+	} else {
+		n = noteBuffer.delNote(n);
+		if (n == 0) mute = true;
+		else setNote(n);
 	}
+	arpController.setLength(noteBuffer.polyphony);
+}
+
+
 };
 
 #endif
